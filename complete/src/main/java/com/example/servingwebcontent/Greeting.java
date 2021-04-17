14def75096af875406d1f6d45c50e708b1b88008
sheng <shengyefe@gmail.com>
2021-04-17 19:23:42 +0200

    finish

---
+++ complete/src/main/java/com/example/servingwebcontent/Greeting.java
@@ -0,0 +1,29 @@
+package com.example.servingwebcontent;
+
+/**
+ * com.example.servingwebcontent
+ * Nombre_project: serving-web-content
+ * Greeting
+ * Created by: sheng
+ * Date : 17/04/2021
+ * Description:
+ **/
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,17 +1,19 @@
 package com.example.servingwebcontent;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 }
